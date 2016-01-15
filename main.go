d5185585f13c77413f8e80c0a76d2e0184020e86
Gen1us2k <minkin.andrew@gmail.com>
2016-01-16 00:55:28 +0600

    More configuration

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"strings"
 	"net/url"
+	"log"
 )
 
 type Config struct {
@@ -19,6 +20,8 @@ type Config struct {
 	SlackChannel string `json:"slack_channel_name"`
 	SlackToken string `json:"slack_token_name"`
 	Interval int64 `json:"check_interval"`
+	ListenPort string `json:"http_port"`
+	IpAddr string `json:"bind_addr"`
 }
 
 type Response struct {
@@ -31,18 +34,20 @@ type Answer struct {
 
 
 func inviteAll(config Config){
+	log.Println("inviteAll")
 	typeFormUrl := fmt.Sprintf("https://api.typeform.com/v0/form/%s?key=%s&completed=true&since=%d", config.TUID, config.TKey, time.Now().Unix() - config.Interval)
-	fmt.Println(typeFormUrl)
 	res, err := http.Get(typeFormUrl)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
 
 	if err != nil {
+		log.Panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
 		if err != nil {
-			fmt.Println(err)
+			log.Panic(err)
 		}
 	}
 
@@ -50,54 +55,54 @@ func inviteAll(config Config){
 	json.Unmarshal(body, &answer)
 
 	for _, user := range answer.Responses {
-		fmt.Printf("Sending invite to %s with email %s", user.Answers[config.NameField] + user.Answers[config.LastNameField], user.Answers[config.EmailField])
+		log.Println("Sending invite to %s %s with email %s", user.Answers[config.NameField], user.Answers[config.LastNameField], user.Answers[config.EmailField])
 		slackUrl := fmt.Sprintf("https://%s.slack.com/api/users.admin.invite", config.SlackChannel)
+
 		invite := url.Values{}
 		invite.Add("email", user.Answers[config.EmailField])
 		invite.Add("first_name", user.Answers[config.NameField])
 		invite.Add("last_name", user.Answers[config.LastNameField])
 		invite.Add("token", config.SlackToken)
 		invite.Add("set_active", "true")
+
 		req, err := http.NewRequest("POST", slackUrl, strings.NewReader(invite.Encode()))
 		if err != nil {
-			fmt.Println(err)
+			log.Panic(err)
 		}
+
 		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		client := &http.Client{}
+
 		resp, err := client.Do(req)
 		if err != nil {
-			panic(err)
+			log.Panic(err)
 		}
+
 		defer resp.Body.Close()
 	}
-
-	time.Sleep(time.Duration(config.Interval) * time.Second)
 }
+
 func main()  {
 	dat, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		fmt.Println("failed to open file config.json")
+		log.Panic("Failed to open file config.json")
 	}
+
 	var config Config
 	err = json.Unmarshal(dat, &config)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Panic(err)
 	}
 
 	go func() {
-		for {
-
+		for _ = range time.Tick(time.Duration(config.Interval) * time.Second) {
 			inviteAll(config)
-
 		}
 	}()
-	startHttpServer()
-}
 
-func startHttpServer(){
 	http.Handle("/", http.FileServer(http.Dir("public")))
-
-	http.ListenAndServe(":3000", nil)
-
+	listenAddr := fmt.Sprintf("%s:%s", config.IpAddr, config.ListenPort)
+	fmt.Println(listenAddr)
+	http.ListenAndServe(listenAddr, nil)
 }
