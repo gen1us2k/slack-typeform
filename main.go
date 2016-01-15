b9ca751491c7f2834fa1b9c332fe2ab627f9cd74
Gen1us2k <minkin.andrew@gmail.com>
2016-01-15 21:32:19 +0600

    Configuration

--- main.go
+++ main.go
@@ -4,9 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"io/ioutil"
+	"encoding/json"
 )
 
+type Config struct {
+	TUID string 	`json:"typeform_uid"`
+	TKey string `json:"typeform_key"`
+	EmailField string `json:"typeform_email_field"`
+	NameField string `json:"typeform_name_field"`
+	LastNameField string `json:"typeform_lastname_field"`
+	SlackChannel string `json:"slack_channel_name"`
+	SlackToken string `json:"slack_token_name"`
+}
+
 func main()  {
+	dat, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println("failed to open file config.json")
+	}
+	var config Config
+	err = json.Unmarshal(dat, &config)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print(config.SlackToken)
+
 	go func() {
 		for {
 
